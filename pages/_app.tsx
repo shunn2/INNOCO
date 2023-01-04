7615b10d304aa6a5031122c5d278cebf67fb5e62
teunkimi-MN <teunkimi-mn@teunkimi-MNui-MacBookPro.local>
2023-01-04 12:58:28 +0900

    :sparkles: [SGSDEVCAMP-1346] style 관련 기본 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,20 @@ import { RecoilRoot } from 'recoil';
 import { Hydrate, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { queryClient } from '@react-query/queryClient';
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/global-style';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+        </ThemeProvider>
         <ReactQueryDevtools position="bottom-right" />
       </QueryClientProvider>
     </RecoilRoot>
---
+++ styles/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+const color = {};
+
+const theme: DefaultTheme = {
+  color,
+};
+
+export default theme;
