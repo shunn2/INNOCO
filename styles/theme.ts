f3d9ff5463e8fe9cca17fa36dcb4ae71014a1bb6
teunkimi-MN <teunkimi-mn@teunkimi-MNui-MacBookPro.local>
2023-01-04 13:53:54 +0900

    :sparkles: [SGSDEVCAMP-1346] 임시 컬러 팔레트 생성

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,13 @@
 import { DefaultTheme } from 'styled-components';
 
-const color = {};
+const color = {
+  blue: '#33ADFF',
+  gray: {
+    dark: '#181c22',
+    middle: '#2C3139',
+    light: '#AFBED1',
+  },
+};
 
 const theme: DefaultTheme = {
   color,
