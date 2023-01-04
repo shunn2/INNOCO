093f46c9d0cbac473a513414d827fc53e6d82406
teunkimi-MN <teunkimi-mn@teunkimi-MNui-MacBookPro.local>
2023-01-04 11:21:37 +0900

    :sparkles: [SGSDEVCAMP-1346] queryClient 설정 및 react query devtools 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import { Hydrate, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { queryClient } from '@react-query/queryClient';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools position="bottom-right" />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
