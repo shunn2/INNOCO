f726b3b24d409aa8d4efc18e374b969167654d43
teunkimi-MN <teunkimi-mn@teunkimi-MNui-MacBookPro.local>
2023-01-09 15:59:08 +0900

    :sparkles: [SGSDEVCAMP-1511] 아이디 유효성 검사 코드 작성

--- utils/validation.ts
+++ utils/validation.ts
@@ -2,3 +2,8 @@ export const validateUserEmail = (email: string) => {
   const regex = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
   return regex.test(email);
 };
+
+export const validateUserId = (id: string) => {
+  const regex = /^[a-z]+[a-z0-9]{5,19}$/g; //영어 소문자+숫자 6~20자리
+  return regex.test(id);
+};
