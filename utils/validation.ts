f67f67f9865c9ba0abae6d536ef9695e98a0725a
teunkimi-MN <teunkimi-mn@teunkimi-MNui-MacBookPro.local>
2023-01-09 16:03:19 +0900

    :sparkles: [SGSDEVCAMP-1511] 비밀번호 유효성 검사 코드 작성

--- utils/validation.ts
+++ utils/validation.ts
@@ -7,3 +7,8 @@ export const validateUserId = (id: string) => {
   const regex = /^[a-z]+[a-z0-9]{5,19}$/g; //영어 소문자+숫자 6~20자리
   return regex.test(id);
 };
+
+export const validateUserPassword = (password: string) => {
+  const regex = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
+  return regex.test(password);
+};
