9186b01253570c86768128505bdbd3c441b55678
조성훈B <shoonchoi@smilegate.sg>
2023-01-09 15:48:10 +0900

    💄 [SGSDEVCAMP-1647] style 컨벤션 수정

--- components/Iframe/index.tsx
+++ components/Iframe/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
 
-import { IframStyle as S } from './styled';
+import { IframStyle as Styled } from './styled';
 
 const CustomIframe = ({ children, ...props }) => {
   const [contentRef, setContentRef] = useState(null);
@@ -9,11 +9,11 @@ const CustomIframe = ({ children, ...props }) => {
   const mountNode = contentRef?.contentWindow?.document?.body;
 
   return (
-    <S.IframeBox>
-      <S.Iframe {...props} ref={setContentRef}>
+    <Styled.IframeBox>
+      <Styled.Iframe {...props} ref={setContentRef}>
         {mountNode && createPortal(children, mountNode)}
-      </S.Iframe>
-    </S.IframeBox>
+      </Styled.Iframe>
+    </Styled.IframeBox>
   );
 };
 
