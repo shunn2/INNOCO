526a1ea24a27009bf4cbec05edb524cd8c6a84ae
teunkimi-MN <teunkimi-mn@teunkimi-MNui-MacBookPro.local>
2023-01-09 16:07:11 +0900

    :truck: [SGSDEVCAMP-1511] auth 디렉토리 생성

--- pages/auth/sign-up/index.tsx
+++ pages/auth/sign-up/index.tsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import { Input, Button } from '@components/Common';
 import { AuthContainer } from '@components/Auth';
+import { validateUserPassword } from '@utils/validation';
 
 const SignUp = () => {
   const [input, setInput] = useState('');
   const _onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
+    console.log(validateUserPassword(input));
   };
 
   return (
     <AuthContainer>
       <p>Sign Up</p>
-      <Input placeholder={'이름을 입력하세요.'} />
-      <Input placeholder={'이메일을 입력하세요.'} onChange={_onChange} />
-      <Input placeholder={'비밀번호를 입력하세요.'} type="password" />
+      <Input placeholder={'이메일을 입력하세요.'} />
+      <Input placeholder={'아이디 입력하세요.'} />
+      <Input
+        placeholder={'비밀번호를 입력하세요.'}
+        type="password"
+        onChange={_onChange}
+      />
       {/* TODO: 이메일 인증 */}
       <Button disabled variant="auth">
         회원가입
