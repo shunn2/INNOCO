45192d09a8df31fee6e22d2c96062a89f901c81f
teunkimi-MN <teunkimi-mn@teunkimi-MNui-MacBookPro.local>
2023-01-17 19:55:06 +0900

    :sparkles: [SGSDEVCAMP-1817] 요청을 보낼때마다 axiosInstance에 Authorization 헤더 추가

--- api/customAxios.ts
+++ api/customAxios.ts
@@ -1,21 +1,33 @@
-import { useSession } from '@lib/next-auth-react-query';
 import axios, { AxiosInstance } from 'axios';
 
 const BASE_URL = process.env.BASE_URL;
-const [session, _] = useSession();
 const HEADERS = {
   'Cache-Control': 'no-cache',
   'Access-Control-Allow-Origin': '*',
   'Content-Type': 'application/json; charset=utf-8',
-  Authorization: `Bearer ${JSON.parse(session.accessToken)}`,
 };
 
-const createAxiosWithToken = (url: string): AxiosInstance => {
-  return axios.create({
-    baseURL: `${BASE_URL}/${url}`,
-    headers: HEADERS,
-    withCredentials: true,
-  });
+const axiosInstanceWithToken = axios.create({
+  baseURL: `${BASE_URL}`,
+  headers: HEADERS,
+  withCredentials: true,
+});
+
+const createAxiosWithToken = (): AxiosInstance => {
+  const accessToken = getToken();
+  setAuthorizationHeader(JSON.parse(accessToken));
+  return axiosInstanceWithToken;
 };
 
-export default createAxiosWithToken;
+const setAuthorizationHeader = (token: string) => {
+  axiosInstanceWithToken.defaults.headers[
+    'Authorization'
+  ] = `Bearer ${JSON.parse(token)}`;
+};
+
+const getToken = () => {
+  const accessToken = window.localStorage.getItem('access_token');
+  return JSON.stringify(accessToken);
+};
+
+export { createAxiosWithToken, setAuthorizationHeader };
