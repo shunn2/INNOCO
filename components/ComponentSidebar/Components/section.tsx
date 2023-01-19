94e6e48143eeeca04ca85cd8e7807b6031e49dee
조성훈B <shoonchoi@smilegate.sg>
2023-01-19 13:39:36 +0900

    ✨ [SGSDEVCAMP-1860] 각 컴포넌트 wrapper로 구조 최신화

--- components/ComponentSidebar/Components/section.tsx
+++ components/ComponentSidebar/Components/section.tsx
@@ -1,40 +1,9 @@
-import { SvgIcon } from '@components/Common';
-import * as Styled from './styled';
-import { v4 as uuidv4 } from 'uuid';
-import dragStart from '@utils/drag/dragStart';
-
-const sectionData = {
-  id: '',
-  type: 'section',
-  tag: 'div',
-  sectionProps: {
-    style: {
-      width: '100%',
-      minHeight: '100px',
-      display: 'flex',
-      backgroundColor: '#fff',
-    },
-    draggable: true,
-    className: ['border-transparent', 'border-4', 'hover:border-cyan-300'],
-  },
-  content: '',
-  class: {
-    flex: true,
-  },
-  image: null,
-  children: [],
-};
+import ComponentWrapper from './componentWrapper';
+import { sectionData } from './data';
 
 const SectionComponent = () => {
-  const handleDragStart = (e) => {
-    sectionData.id = uuidv4();
-    dragStart({ e: e, element: sectionData, idx: '', sectionId: '' });
-  };
   return (
-    <Styled.ComponentItems draggable onDragStart={handleDragStart}>
-      <SvgIcon type="box-icon" />
-      <div>Section</div>
-    </Styled.ComponentItems>
+    <ComponentWrapper type="Section" icon={'box-icon'} data={sectionData} />
   );
 };
 
