0797367b40badb85b719082a0df22268a3b079d4
조성훈B <shoonchoi@smilegate.sg>
2023-01-19 13:54:49 +0900

    🎨 [SGSDEVCAMP-1860] blue theme 적용

--- components/ComponentSidebar/styled.ts
+++ components/ComponentSidebar/styled.ts
@@ -4,6 +4,6 @@ import theme from '@styles/theme';
 export const ComponentSidebar = styled.div`
   width: 166px;
   height: 100%;
-  background-color: ${theme.color.gray.middle};
-  color: ${theme.color.blue};
+  background-color: ${theme.color.blue.dark};
+  color: ${theme.color.blue.middle};
 `;
