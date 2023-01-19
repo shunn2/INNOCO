34993e767f705989ecf570d8a59cb53c2aa5906a
조성훈B <shoonchoi@smilegate.sg>
2023-01-19 10:24:20 +0900

    🎨 [SGSDEVCAMP-1826] iframe return 함수 적용

--- components/Editor/Customframe/index.tsx
+++ components/Editor/Customframe/index.tsx
@@ -1,3 +1,4 @@
+import { IframeEditorReturn } from '@utils/iframe/iframeEditorReturn';
 import { PropsWithChildren, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { IframStyle as Styled } from './styled';
@@ -17,24 +18,14 @@ const CustomIframe = ({
   const mountNode = contentRef?.contentWindow?.document?.body;
 
   useEffect(() => {
-    const isIFrame = (prop: HTMLElement | null): prop is HTMLIFrameElement =>
-      prop !== null && prop.tagName === 'IFRAME';
-    const frame = document.getElementById('editor_iframe');
-    if (isIFrame(frame)) {
-      let script = document.createElement('script');
-      script.src = 'https://cdn.tailwindcss.com';
-      let link = document.createElement('link');
-      link.href = 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css';
-      link.rel = 'stylesheet';
-      frame.contentWindow.document.head.appendChild(script);
-      frame.contentWindow.document.head.appendChild(link);
-      // let doc = frame.contentDocument;
-      // doc.head.appendChild(script);
-      // let doc = frame.contentDocument;
-      // doc.body.innerHTML =
-      //   doc.body.innerHTML +
-      //   `<style>.element_event:hover{border: 4px solid #0077CC;} .element_event:visited{border: 4px solid #0077CC;}</style>`;
-    }
+    const frame = IframeEditorReturn();
+    let script = document.createElement('script');
+    script.src = 'https://cdn.tailwindcss.com';
+    let link = document.createElement('link');
+    link.href = 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css';
+    link.rel = 'stylesheet';
+    frame.contentWindow.document.head.appendChild(script);
+    frame.contentWindow.document.head.appendChild(link);
   }, [contentRef]);
 
   return (
