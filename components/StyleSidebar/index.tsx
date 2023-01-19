db30c1e14d4a23f8624f25bfc2bf132b04718723
조성훈B <shoonchoi@smilegate.sg>
2023-01-19 16:32:05 +0900

    ✨ [SGSDEVCAMP-1867] 컴포넌트 미클릭 시 style 변경 방지

--- components/StyleSidebar/index.tsx
+++ components/StyleSidebar/index.tsx
@@ -3,17 +3,39 @@ import * as Styled from './styled';
 import ColorMenu from './ColorMenu';
 import ToggleMenu from './ToggleMenu';
 import BackgroundColorMenu from './BackgroundMenu';
+import { useRecoilValue } from 'recoil';
+import { elementInfoAtom } from '@recoil/styleSideBar/atom';
+import ComponentInfo from './ComponentInfo';
 
 const StyleSidebar = () => {
+  const clickedComponent = useRecoilValue(elementInfoAtom);
   return (
-    <Styled.StyledSidebar>
-      <ToggleMenu title="Color">
-        <ColorMenu />
-      </ToggleMenu>
-      <ToggleMenu title="Background">
-        <BackgroundColorMenu />
-      </ToggleMenu>
-    </Styled.StyledSidebar>
+    <Styled.StyleSidebar>
+      {clickedComponent.id ? (
+        <>
+          <ComponentInfo />
+          <ToggleMenu title="Color">
+            <ColorMenu />
+          </ToggleMenu>
+          <ToggleMenu title="Background">
+            <BackgroundColorMenu />
+          </ToggleMenu>
+        </>
+      ) : (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '24px',
+          }}
+        >
+          Click Component!
+        </div>
+      )}
+    </Styled.StyleSidebar>
   );
 };
 
