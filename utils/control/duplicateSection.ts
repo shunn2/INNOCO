43e595e984f2ac0d4d00463ae63df171ade1c64e
조성훈B <shoonchoi@smilegate.sg>
2023-01-25 10:58:04 +0900

    ✨ [SGSDEVCAMP-1933] section 복제 기능 추가

---
+++ utils/control/duplicateSection.ts
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const duplicateSection = (e, element, main, setMain, setSectionOrder) => {
+  const duplicated = { ...main[element.id] };
+  duplicated.id = uuidv4();
+  setMain((prev) => {
+    let cur = JSON.parse(JSON.stringify(prev));
+    cur[duplicated.id] = duplicated;
+    return cur;
+  });
+  setSectionOrder((prev) => {
+    let cur = [...prev];
+    cur.splice(element.index + 1, 0, duplicated.id);
+    return cur;
+  });
+  e.stopPropagation();
+};
+
+export default duplicateSection;
