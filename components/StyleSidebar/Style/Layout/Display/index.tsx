0a0d4c4d2b11a9565084c8d75f8d6e1378abbed7
조성훈B <shoonchoi@smilegate.sg>
2023-01-26 14:36:32 +0900

    🐛 [SGSDEVCAMP-1978] 초기 display 값 렌더링 bug fix

--- components/StyleSidebar/Style/Layout/Display/index.tsx
+++ components/StyleSidebar/Style/Layout/Display/index.tsx
@@ -12,19 +12,16 @@ import FlexMenu from './Flex';
 const displayList = ['block', 'flex', 'grid', 'inline-block', 'inline', 'none'];
 
 const DisplayMenu = () => {
-  const [display, setDisplay] = useState<string>();
-
   const element = useRecoilValue(elementInfoAtom);
   const [mainData, setMainData] = useRecoilState(withMainData);
+  const [display, setDisplay] = useState<string>(
+    getCurrentStyle({ element, type: 'display', mainData }) || 'block'
+  );
 
   const handleDisplayChange = (value) => {
     setDisplay(value);
     styleChange({ element, type: 'display', value, setMainData });
   };
-
-  useEffect(() => {
-    setDisplay(getCurrentStyle({ element, type: 'display', mainData }) || '');
-  }, [element, mainData]);
 
   return (
     <Styled.StyleBox>
