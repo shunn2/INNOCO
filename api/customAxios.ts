e5c24348ee7e9f58cf0e2e8667c61660bec81e99
조성훈B <shoonchoi@smilegate.sg>
2023-01-27 13:12:28 +0900

    👽️ [SGSDEVCAMP-2001] without token header 변경

--- api/customAxios.ts
+++ api/customAxios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from 'axios';
 const BASE_URL = process.env.BASE_URL;
 const HEADERS = {
   'Cache-Control': 'no-cache',
-  'Access-Control-Allow-Origin': '*',
+  // 'Access-Control-Allow-Origin': '*',
   'Content-Type': 'application/json; charset=utf-8',
 };
 
