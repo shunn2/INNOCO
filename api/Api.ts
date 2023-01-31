7a264470dff500911ad625d06f8be834d3663dd9
조성훈B <shoonchoi@smilegate.sg>
2023-01-31 17:59:44 +0900

    ✨ [SGSDEVCAMP-2059] project delete api 생성

--- api/Api.ts
+++ api/Api.ts
@@ -5,6 +5,17 @@ class Api {
     const { data } = await createAxiosWithToken().get('/projects/list/me');
     return data;
   }
+  async fetchTemplates() {
+    const { data } = await createAxiosWithToken().get('/templates');
+    console.log(data);
+    return data;
+  }
+  async deleteProject(projectId) {
+    const { data } = await createAxiosWithToken().delete(
+      `/projects/${projectId}`
+    );
+    return data;
+  }
   async postImage(multipartFile) {
     const { data } = await createAxiosWithToken().post(
       '/image/upload',
