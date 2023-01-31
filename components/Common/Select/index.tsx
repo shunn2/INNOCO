0b35ac63184ac878db871a384980d01482360d63
조성훈B <shoonchoi@smilegate.sg>
2023-01-31 15:44:06 +0900

    ✨ [SGSDEVCAMP-2051] Select Common 컴포넌트 구현

---
+++ components/Common/Select/index.tsx
@@ -0,0 +1,33 @@
+import { ChangeEvent } from 'react';
+import * as Styled from './styled';
+
+interface OptionProps {
+  title: string;
+  value: string;
+}
+
+interface SelectProps {
+  optionList: OptionProps[];
+  selected: string;
+  size?: number;
+  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const Select = (props: SelectProps) => {
+  const { optionList, selected, size, onChange } = props;
+  return (
+    <Styled.Select size={size} onChange={(e) => onChange(e)}>
+      {optionList.map((option) => (
+        <Styled.Option
+          value={option.value}
+          selected={selected === option.value}
+          key={option.title}
+        >
+          {option.title}
+        </Styled.Option>
+      ))}
+    </Styled.Select>
+  );
+};
+
+export default Select;
