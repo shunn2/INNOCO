a904ccc894b7ab52e1e6c5b25c405a50450127f3
조성훈B <shoonchoi@smilegate.sg>
2023-02-02 16:28:19 +0900

    ✨ [SGSDEVCAMP-2068] 프로젝트 생성 모달에 main page 생성 폼 추가

--- components/Dashboard/CreateProject/index.tsx
+++ components/Dashboard/CreateProject/index.tsx
@@ -1,9 +1,15 @@
+import { api } from '@api';
 import { createAxiosWithToken } from '@api/customAxios';
 import { Input } from '@components/Common';
 import ImageUpload from '@components/Common/ImageUpload';
 import CreateModal from '@components/Common/Modal';
-import { useState } from 'react';
+import useTemplates from '@hooks/useTemplates';
+import queryKeys from '@react-query/queryKeys';
+import { Templates } from '@/types/template';
+import { useEffect, useState } from 'react';
+import { dehydrate, QueryClient, useQuery } from 'react-query';
 import * as Styled from './styled';
+import pageApi from '@api/pageApi';
 interface CreateProjectProps {
   isOpen: boolean;
   handleIsOpen: () => void;
@@ -12,18 +18,29 @@ interface CreateProjectProps {
 const CreateProject = ({ isOpen, handleIsOpen }: CreateProjectProps) => {
   const [projectName, setProjectName] = useState<string>();
   const [projectThumbnail, setProjectThumbnail] = useState<string>();
+  const [mainPageName, setMainPageName] = useState<string>();
+  const [templateId, setTemplateId] = useState<string>();
+
+  const [createDisabled, setCreateDisabled] = useState<boolean>(true);
+
+  const templates: Templates = useTemplates();
 
   const createProject = async () => {
-    const { data } = await createAxiosWithToken().post('/projects', {
-      projectName: projectName,
-      projectThumbnailUrl: projectThumbnail,
-    });
+    await api
+      .createProject(projectName, projectThumbnail)
+      .then((data) => pageApi.createPage(data.value, mainPageName, templateId));
     handleIsOpen();
     location.reload();
   };
 
+  const handleTemplateId = (id) => {
+    setTemplateId(id);
+  };
   const handleProjectName = (e) => {
     setProjectName(e.target.value);
+  };
+  const handleMainPageName = (e) => {
+    setMainPageName(e.target.value);
   };
 
   return (
@@ -35,7 +52,7 @@ const CreateProject = ({ isOpen, handleIsOpen }: CreateProjectProps) => {
       <Styled.InputWrapper>
         <Styled.InputLabel>Project Name</Styled.InputLabel>
         <Input
-          placeholder="type project name"
+          placeholder="project name"
           size={300}
           onChange={handleProjectName}
         />
@@ -44,16 +61,75 @@ const CreateProject = ({ isOpen, handleIsOpen }: CreateProjectProps) => {
         <Styled.InputLabel>Project Thumbnail</Styled.InputLabel>
         <Styled.ThumbnailWrapper>
           <ImageUpload type="thumbnail" setURL={setProjectThumbnail} />
-          <Styled.Thumbnail src={projectThumbnail} />
+          <Styled.Thumbnail
+            className="ml-9"
+            src={projectThumbnail}
+            width={300}
+            height={240}
+          />
         </Styled.ThumbnailWrapper>
       </Styled.InputWrapper>
+      <Styled.InputWrapper>
+        <Styled.InputLabel>Main Page Name</Styled.InputLabel>
+        <Input
+          placeholder="main page name"
+          size={300}
+          onChange={handleMainPageName}
+        />
+      </Styled.InputWrapper>
+      <div>
+        <Styled.InputLabel>Project Thumbnail</Styled.InputLabel>
+        <Styled.TemplateContainer>
+          <Styled.Thumbnail
+            src={''}
+            width={250}
+            height={250}
+            alt="Blank"
+            onClick={() => handleTemplateId('')}
+          />
+          {templates?.value.map((template, idx) => (
+            <Styled.TemplateWrapper
+              key={template.templateId}
+              selected={template.templateId === templateId}
+            >
+              <Styled.Thumbnail
+                src={template.templateThumbnailUrl}
+                width={250}
+                height={250}
+                onClick={() => handleTemplateId(template.templateId)}
+              />
+              <Styled.ThumbnailTitle>
+                {template.templateName}
+              </Styled.ThumbnailTitle>
+            </Styled.TemplateWrapper>
+          ))}
+        </Styled.TemplateContainer>
+      </div>
       <Styled.ButtonWrapper>
-        <Styled.SubmitButton onClick={() => createProject()}>
+        <Styled.SubmitButton
+          onClick={() => createProject()}
+          disabled={createDisabled}
+        >
           Create
         </Styled.SubmitButton>
       </Styled.ButtonWrapper>
     </CreateModal>
   );
+};
+
+export const getServerSideProp = async (context) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(queryKeys.templates, () =>
+    api.fetchTemplates()
+  );
+  const dehydratedState = dehydrate(queryClient);
+
+  return {
+    props: {
+      dehydratedState: dehydratedState,
+    },
+  };
 };
 
 export default CreateProject;
