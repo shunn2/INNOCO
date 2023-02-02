f7a95584e43a93cf1f91aba079cb50cc35bb1cd5
조성훈B <shoonchoi@smilegate.sg>
2023-02-02 16:28:41 +0900

    💄 [SGSDEVCAMP-2068] thumbnail UI 개선

--- components/Dashboard/CreateProject/styled.ts
+++ components/Dashboard/CreateProject/styled.ts
@@ -13,7 +13,7 @@ export const CloseButton = styled.div`
   cursor: pointer;
 `;
 
-export const SubmitButton = styled.div`
+export const SubmitButton = styled.button`
   background-color: ${theme.color.blue.middle};
   padding: 6px 10px;
   border-radius: 4px;
@@ -43,8 +43,25 @@ export const ThumbnailWrapper = styled.div`
   margin-top: 16px;
 `;
 
-export const Thumbnail = styled.img`
-  width: 300px;
-  height: 240px;
-  margin-left: 36px;
+export const Thumbnail = styled.img<{ width: number; height: number }>`
+  width: ${(props) => `${props.width}px`};
+  height: ${(props) => `${props.height}px`};
+`;
+
+export const ThumbnailTitle = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 8px 0;
+`;
+
+export const TemplateContainer = styled.div`
+  width: 100%;
+  display: flex;
+  overflow-x: scroll;
+`;
+
+export const TemplateWrapper = styled.div<{ selected?: boolean }>`
+  border: ${(props) =>
+    props.selected ? `2px solid ${theme.color.blue.middle}` : ''};
 `;
