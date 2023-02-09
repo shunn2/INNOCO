7f3398e30c2f067005fc1e06111a6e09fbd53826
조성훈B <shoonchoi@smilegate.com>
2023-02-09 15:45:14 +0900

    ✨ [SGSDEVCAMP-2128] api interceptor 구현

--- api/customAxios.ts
+++ api/customAxios.ts
@@ -1,4 +1,14 @@
 import axios, { AxiosInstance } from 'axios';
+import jwt_decode from 'jwt-decode';
+import authApi from './authApi';
+
+interface AuthToken {
+  sub: string;
+  role: string;
+  'token-type': string;
+  iat: number;
+  exp: number;
+}
 
 const BASE_URL = process.env.BASE_URL;
 const HEADERS = {
@@ -24,8 +34,29 @@ const setAuthorizationHeader = (token: string) => {
 };
 
 const getToken = () => {
-  const accessToken = window.localStorage.getItem('access_token');
+  const accessToken = localStorage.getItem('access_token');
   return accessToken;
 };
+
+axiosInstanceWithToken.interceptors.request.use(
+  async (config) => {
+    if (!config.headers) return config;
+    const access_token = localStorage.getItem('access_token');
+    const refresh_token = JSON.parse(localStorage.getItem('refresh_token'));
+    const decodedAccess: AuthToken = jwt_decode(access_token);
+
+    if (decodedAccess.exp < Date.now() / 1000) {
+      const res = await authApi.regenerateToken(refresh_token);
+      const { accessToken, refreshToken } = res.value;
+      localStorage.setItem('access_token', JSON.stringify(accessToken));
+      localStorage.setItem('refresh_token', JSON.stringify(refreshToken));
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export { createAxiosWithToken, setAuthorizationHeader };
