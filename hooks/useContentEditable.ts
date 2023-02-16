31160baf2972e066884693b8153853a49ab3579c
조성훈B <shoonchoi@smilegate.com>
2023-02-16 11:22:16 +0900

    🚨 [SGSDEVCAMP-2295] hook naming 수정

--- hooks/useContentEditable.ts
+++ hooks/useContentEditable.ts
@@ -1,7 +1,9 @@
-export const useContentEditable = (e, elIdx, sectionId, setMainData) => {
+const useContentEditable = (e, elIdx, sectionId, setMainData) => {
   setMainData((prev) => {
     const cur = JSON.parse(JSON.stringify(prev));
     cur[sectionId].children[elIdx].content = e.target.innerHTML;
     return cur;
   });
 };
+
+export default useContentEditable;
