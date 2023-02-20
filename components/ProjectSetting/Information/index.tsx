e896f98bc91e4f20602d2fcc5141cddc83eefb3b
조성훈B <shoonchoi@smilegate.com>
2023-02-20 14:12:56 +0900

    ✨ [SGSDEVCAMP-2335] 프로젝트 정보 수정 기능 구현

---
+++ components/ProjectSetting/Information/index.tsx
@@ -0,0 +1,105 @@
+import { api } from '@api';
+import editApi from '@api/editApi';
+import { Select } from '@components/Common';
+import Alert from '@components/Common/Alert';
+import ImageUpload from '@components/Common/ImageUpload';
+import StyleInput from '@components/Common/StyleInput';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import * as Styled from './styled';
+
+interface ProjectInfo {
+  projectThumbnailUrl: string;
+  projectName: string;
+  projectStatus: string;
+}
+
+const statusOption = [
+  { title: '공개', value: 'PUBLIC' },
+  { title: '비공개', value: 'PRIVATE' },
+  // { title: '진행중', value: '진행중' },
+];
+
+const InformationSetting = () => {
+  const projectId = useRouter().query.projectId;
+  const [projectInformation, setProjectInformation] = useState<ProjectInfo>({
+    projectThumbnailUrl: '',
+    projectName: '',
+    projectStatus: '',
+  });
+  const handleProjectInformation = (type, value) => {
+    setProjectInformation({ ...projectInformation, [type]: value });
+  };
+  const getProjectInfo = async () => {
+    const data = await api.fetchSingleProject(projectId);
+    setProjectInformation(data.value);
+  };
+  const editProjectInfo = async () => {
+    const data = await editApi.editProjectInformation(
+      projectId,
+      projectInformation
+    );
+    Alert({ icon: 'success', title: '수정이 완료되었습니다.' });
+  };
+  useEffect(() => {
+    if (projectId) getProjectInfo();
+  }, [projectId]);
+  return (
+    <Styled.InformationContainer>
+      <Styled.ThumbnailContainer>
+        <Styled.InputLabel>Project Thumbnail</Styled.InputLabel>
+        <Styled.ThumbnailWrapper>
+          <Styled.Thumbnail
+            src={projectInformation.projectThumbnailUrl}
+            width={600}
+            height={480}
+          />
+          <ImageUpload
+            type="thumbnail"
+            setURL={(url) =>
+              handleProjectInformation('projectThumbnailUrl', url)
+            }
+          />
+        </Styled.ThumbnailWrapper>
+      </Styled.ThumbnailContainer>
+      <Styled.ProjectInfoContainer>
+        <div>
+          <Styled.InputLabel>Project Name</Styled.InputLabel>
+          <StyleInput
+            placeholder={projectInformation.projectName}
+            value={projectInformation.projectName}
+            size={80}
+            height={50}
+            onChange={(e) =>
+              handleProjectInformation('projectName', e.target.value)
+            }
+          />
+        </div>
+        <div>
+          <Styled.InputLabel className="mt-32">
+            Project Status
+          </Styled.InputLabel>
+          <Styled.StatusSelect
+            defaultValue={projectInformation.projectStatus}
+            onChange={(e) =>
+              handleProjectInformation('projectStatus', e.target.value)
+            }
+          >
+            {statusOption.map((option) => (
+              <Styled.StatusOption key={option.value} value={option.value}>
+                {option.title}
+              </Styled.StatusOption>
+            ))}
+          </Styled.StatusSelect>
+        </div>
+        <Styled.ButtonWrapper>
+          <Styled.ChangeButton onClick={() => editProjectInfo()}>
+            수정하기
+          </Styled.ChangeButton>
+        </Styled.ButtonWrapper>
+      </Styled.ProjectInfoContainer>
+    </Styled.InformationContainer>
+  );
+};
+
+export default InformationSetting;
