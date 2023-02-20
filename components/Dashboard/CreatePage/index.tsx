8945872cef1dafad81e2ec7287d7e764e96f316d
조성훈B <shoonchoi@smilegate.com>
2023-02-20 14:27:26 +0900

    ✨ [SGSDEVCAMP-2338] 페이지 생성 모달

---
+++ components/Dashboard/CreatePage/index.tsx
@@ -0,0 +1,105 @@
+import { Templates } from '@/types/template';
+import pageApi from '@api/pageApi';
+import { Input } from '@components/Common';
+import Alert from '@components/Common/Alert';
+import useTemplates from '@hooks/useTemplates';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import * as Styled from './styled';
+
+interface CreatePageProps {
+  handleOpen: () => void;
+}
+
+const CreatePage = ({ handleOpen }: CreatePageProps) => {
+  const router = useRouter();
+  const projectId = useRouter().query.projectId;
+  const templates: Templates = useTemplates();
+  const [pageName, setPageName] = useState<string>('');
+  const [templateId, setTemplateId] = useState<string>('');
+
+  const handlePageName = (e) => {
+    setPageName(e.target.value);
+  };
+  const handleTemplateId = (id) => {
+    setTemplateId(id);
+  };
+  const validateCreateButton = () => {
+    //page name 중복 체크 추가 예정
+    if (pageName.length) return false;
+    return true;
+  };
+  const createPage = async () => {
+    const data = await pageApi.createPage(projectId, pageName, templateId);
+    if (data.code === 0) {
+      router.replace({
+        pathname: '/editor/[projectId]/[pageId]',
+        query: { projectId: projectId, pageId: data.value },
+      });
+      router.reload();
+      handleOpen();
+    } else
+      Alert({
+        icon: 'warning',
+        title: '페이지 이름이 중복됩니다.',
+      });
+  };
+  return (
+    <>
+      <Styled.ButtonWrapper>
+        <Styled.CloseButton onClick={handleOpen}>x</Styled.CloseButton>
+      </Styled.ButtonWrapper>
+      <Styled.Title>Make your Project</Styled.Title>
+      <Styled.InputWrapper>
+        <Styled.InputLabel>Page Name</Styled.InputLabel>
+        <Input
+          placeholder="page name"
+          value={pageName}
+          size={300}
+          onChange={handlePageName}
+        />
+      </Styled.InputWrapper>
+      <div>
+        <Styled.InputLabel>Page Template</Styled.InputLabel>
+        <Styled.TemplateContainer>
+          <Styled.TemplateWrapper
+            selected={templateId === ''}
+            onClick={() => handleTemplateId('')}
+          >
+            <Styled.Thumbnail
+              src={null}
+              width={250}
+              height={250}
+              selected={templateId === ''}
+            />
+            <Styled.ThumbnailTitle>Blank</Styled.ThumbnailTitle>
+          </Styled.TemplateWrapper>
+          {templates?.value.map((template, idx) => (
+            <Styled.TemplateWrapper key={template.templateId}>
+              <Styled.Thumbnail
+                src={template.templateThumbnailUrl}
+                width={250}
+                height={250}
+                onClick={() => handleTemplateId(template.templateId)}
+                selected={templateId === template.templateId}
+              />
+              <Styled.ThumbnailTitle>
+                {template.templateName}
+              </Styled.ThumbnailTitle>
+            </Styled.TemplateWrapper>
+          ))}
+        </Styled.TemplateContainer>
+        <Styled.ButtonWrapper className="mt-12">
+          <Styled.SubmitButton
+            disabled={validateCreateButton()}
+            onClick={() => createPage()}
+          >
+            Create
+          </Styled.SubmitButton>
+        </Styled.ButtonWrapper>
+      </div>
+    </>
+  );
+};
+
+export default CreatePage;
---
+++ components/Dashboard/CreatePage/styled.ts
@@ -0,0 +1,85 @@
+import theme from '@styles/theme';
+import styled from 'styled-components';
+
+export const ProjectModalContainer = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const CloseButton = styled.div`
+  font-size: 24px;
+  padding: 4px;
+  margin-bottom: 16px;
+  cursor: pointer;
+`;
+
+export const SubmitButton = styled.button<{ disabled: boolean }>`
+  background-color: ${(props) =>
+    props.disabled ? theme.color.gray.middle : theme.color.blue.middle};
+  padding: 6px 10px;
+  border-radius: 4px;
+  cursor: pointer;
+  pointer-events: ${(props) => props.disabled && 'none'};
+  &:hover {
+    background-color: ${theme.color.blue.light};
+  }
+`;
+
+export const Title = styled.div`
+  font-size: 20px;
+  margin-bottom: 14px;
+  color: ${theme.color.gray.light};
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  align-items: center;
+  margin: 16px 0;
+  color: ${theme.color.gray.light};
+`;
+
+export const InputLabel = styled.div`
+  font-size: 12px;
+  margin: 8px 0 12px 4px;
+  color: ${theme.color.gray.light};
+`;
+
+export const ThumbnailWrapper = styled.div`
+  display: flex;
+  margin-top: 16px;
+`;
+
+export const Thumbnail = styled.img<{
+  width: number;
+  height: number;
+  selected: boolean;
+}>`
+  width: ${(props) => `${props.width}px`};
+  height: ${(props) => `${props.height}px`};
+  border: 0.5px solid ${theme.color.unique.side};
+  border: ${(props) =>
+    props.selected ? `2px solid ${theme.color.blue.middle}` : ''};
+`;
+
+export const ThumbnailTitle = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 8px 0;
+`;
+
+export const TemplateContainer = styled.div`
+  width: 100%;
+  display: flex;
+  overflow-x: scroll;
+`;
+
+export const TemplateWrapper = styled.div<{ selected?: boolean }>``;
