67aa958ce61ca208fa2287642016df260fb778c7
조성훈B <shoonchoi@smilegate.com>
2023-02-28 17:16:23 +0900

    ✨ [SGSDEVCAMP-2505] interceptor request->response

--- api/customAxios.ts
+++ api/customAxios.ts
@@ -1,5 +1,8 @@
+import { userInfoAtom } from '@recoil/user/atom';
 import axios, { AxiosInstance } from 'axios';
 import jwt_decode from 'jwt-decode';
+import { useRouter } from 'next/router';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
 import authApi from './authApi';
 
 interface AuthToken {
@@ -45,8 +48,22 @@ const getToken = () => {
 //     const access_token = localStorage.getItem('access_token');
 //     const refresh_token = JSON.parse(localStorage.getItem('refresh_token'));
 //     const decodedAccess: AuthToken = jwt_decode(access_token);
+//     const decodedRefresh: AuthToken = jwt_decode(refresh_token);
+//     console.log('decodedrefresh', decodedRefresh);
+
+//     if (decodedRefresh.exp < Date.now() / 1000) {
+//       console.log(11111);
+
+//       localStorage.removeItem('access_token');
+//       location.replace('/auth/sign-in');
+//       return;
+//     }
 //     if (decodedAccess.exp < Date.now() / 1000) {
+//       console.log(111);
+
 //       const res = await authApi.regenerateToken(refresh_token);
+//       console.log('regenerate', res);
+
 //       const { accessToken, refreshToken } = res.value;
 //       localStorage.setItem('access_token', JSON.stringify(accessToken));
 //       localStorage.setItem('refresh_token', JSON.stringify(refreshToken));
@@ -58,6 +75,22 @@ const getToken = () => {
 //     return Promise.reject(error);
 //   }
 // );
+let isAlreadyFetchingAccessToken = false;
+let pendingRequest = [];
+
+const addPendingRequest = (callback) => {
+  pendingRequest.push(callback);
+};
+
+const onAccessTokenFetched = (accessToken) => {
+  pendingRequest.forEach((callback) => callback(accessToken));
+  pendingRequest = [];
+};
+
+const signOut = () => {
+  localStorage.removeItem('access_token');
+  window.location.replace('/auth/sign-in');
+};
 
 axiosInstanceWithToken.interceptors.response.use(
   (response) => {
@@ -68,20 +101,44 @@ axiosInstanceWithToken.interceptors.response.use(
       config,
       response: { status },
     } = error;
-    if (status === 401) {
-      const originalRequest = config;
-      const refresh_token = await localStorage.getItem('refresh_token');
-      const res = await authApi.regenerateToken(refresh_token);
-      console.log('res', res);
-      const { accessToken, refreshToken } = res.value;
-      await localStorage.multiSet([
-        ['access_token', accessToken],
-        ['refresh_token', refreshToken],
-      ]);
-      originalRequest.headers.authorization = `Bearer ${accessToken}`;
-      return axios(originalRequest);
+
+    if (status === 401 || status === 403) {
+      try {
+        const originalRequest = config;
+
+        const retryOriginalRequest = new Promise((resolve, reject) => {
+          addPendingRequest(async (accessToken) => {
+            try {
+              config.headers['Authorization'] = 'Bearer ' + accessToken;
+              resolve(axios(originalRequest));
+            } catch (err) {
+              reject(err);
+            }
+          });
+        });
+
+        const refresh_token = JSON.parse(localStorage.getItem('refresh_token'));
+        if (!isAlreadyFetchingAccessToken) {
+          isAlreadyFetchingAccessToken = true;
+
+          const res = await authApi.regenerateToken(refresh_token);
+          if (!res.value) {
+            signOut();
+            return;
+          }
+
+          const { accessToken, refreshToken } = res.value;
+          localStorage.setItem('access_token', JSON.stringify(accessToken));
+          localStorage.setItem('refresh_token', JSON.stringify(refreshToken));
+          isAlreadyFetchingAccessToken = false;
+          onAccessTokenFetched(accessToken);
+        }
+        return retryOriginalRequest;
+      } catch (err) {
+        signOut();
+        return Promise.reject(err);
+      }
     }
-    return Promise.reject(error);
   }
 );
 
