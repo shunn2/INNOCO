1b77527ddf0f4661d012973d7ba44cd12437bc01
조성훈B <shoonchoi@smilegate.com>
2023-03-02 16:43:29 +0900

    ✨ editor 표시

--- components/Common/UserAvatar/index.tsx
+++ components/Common/UserAvatar/index.tsx
@@ -12,13 +12,21 @@ const colors = ['red', 'green', 'blue', 'orange', 'yellow'];
 
 const UserAvatar = ({ user, idx }: { user: UserProps; idx: number }) => {
   return (
-    <Avatar
-      name={user.userId}
-      size="30"
-      color={colors[idx]}
-      textSizeRatio={2}
-      round="50%"
-    />
+    <div className="flex flex-col">
+      {/* {user.authority === 'EDITOR' && (
+        <div className="-mb-4 z-10">
+          <img src="/editor_crown.png" />
+        </div>
+      )} */}
+      <Avatar
+        name={user.userId}
+        fgColor={user.authority === 'EDITOR' ? 'black' : '#fff'}
+        size="30"
+        color={colors[idx]}
+        textSizeRatio={2}
+        round="50%"
+      />
+    </div>
   );
 };
 
